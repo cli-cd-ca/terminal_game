d3c33b38d68a7e0a30cc5e44566b69258bab8dfe
cli-cd-ca <88470982+cli-cd-ca@users.noreply.github.com>
2021-08-08 05:16:23 -0400

    Add large square boards and import in game file

--- match_shapes_game.py
+++ match_shapes_game.py
@@ -1,3 +1,3 @@
-from gameboards import print_board, start_game_board, larg_tri_boards, small_tri_boards
+from gameboards import print_board, start_game_board, larg_tri_boards, small_tri_boards, larg_squ_boards
 
-print_board(small_tri_boards[15])
+print_board(larg_squ_boards[15])
