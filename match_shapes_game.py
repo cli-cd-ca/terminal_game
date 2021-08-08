fc7189bf892cc53b3377dec4484a37260050ccb9
cli-cd-ca <88470982+cli-cd-ca@users.noreply.github.com>
2021-08-08 05:03:25 -0400

    Add small triangle boards and import in game file

--- match_shapes_game.py
+++ match_shapes_game.py
@@ -1,3 +1,3 @@
-from gameboards import print_board, start_game_board, larg_tri_boards
+from gameboards import print_board, start_game_board, larg_tri_boards, small_tri_boards
 
-print_board(larg_tri_boards[15])
+print_board(small_tri_boards[15])
