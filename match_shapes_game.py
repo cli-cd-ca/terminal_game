8c51e8084b4bfe8148a3a18fec9e7ecc7f3a6e98
cli-cd-ca <88470982+cli-cd-ca@users.noreply.github.com>
2021-08-08 07:46:56 -0400

    Add README file and edit game file description

--- match_shapes_game.py
+++ match_shapes_game.py
@@ -1,4 +1,4 @@
-# Matching images memory game with shapes in 4x4 board where player flips pieces using row and column number until they match all the shapes
+# Match the images memory game with shapes in 4x4 board where player flips pieces using row and column number until they match all the shapes
 
 from gameboards import print_board, start_game_board, larg_tri_boards, small_tri_boards, larg_squ_boards, small_squ_boards, larg_paral_boards, small_paral_boards, larg_diamd_boards, small_diamd_boards, game_boards_dict
 
